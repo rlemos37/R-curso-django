a0821783f45d96d2fb143fdb5a5e32cd18f25c6b
rlemos37 <lemos_007_@hotmail.com>
2020-11-27 12:51:59 -0300

    Adicionado vídeo de acordo com Slug.

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    videos={
-        'motivacao':{'titulo': 'Vídeo Aperitivo: Motivação', 'vimeo_id': 477256996},
-        'instalacao-windows':{'titulo': 'Instalação Windows', 'vimeo_id': 251497668},
+    videos = {
+        'motivacao': {'titulo': 'Vídeo Aperitivo: Motivação', 'vimeo_id': 477256996},
+        'instalacao-windows': {'titulo': 'Instalação Windows', 'vimeo_id': 251497668},
     }
     video = videos[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
